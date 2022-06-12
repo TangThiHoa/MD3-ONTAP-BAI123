d25c9d1838c656bda03b8c31f39fef0d77a24785
TangThiHoa <hoarua592001@gmail.com>
2022-06-13 00:19:48 +0700

    xong manage con phan chuyen tien

---
+++ src/main/java/com/chuyentien/controller/ChuyenTienController.java
@@ -0,0 +1,24 @@
+package com.chuyentien.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ChuyenTienController {
+    @GetMapping("/chuyentien")
+    public ModelAndView showForm() {
+        ModelAndView modelAndView = new ModelAndView("chuyentien");
+        return modelAndView;
+    }
+
+    @PostMapping("/chuyentien")
+    public ModelAndView chuyentien(@RequestParam("usd") int usd) {
+        ModelAndView modelAndView = new ModelAndView("/chuyentien");
+        int vnd = usd * 2300;
+        modelAndView.addObject("vnd", vnd);
+        return modelAndView;
+    }
+}
